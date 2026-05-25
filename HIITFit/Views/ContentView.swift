e49d0efa8e0931e13e6cd8a83e7102d8cb5f0193
FoziaAkhtar <fozia.shah0001@gmail.com>
2026-05-24 20:25:08 -0400

    Grouped views

---
+++ HIITFit/Views/ContentView.swift
@@ -0,0 +1,18 @@
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+     TabView{
+         WelcomeView()      // was Text("Wlcome!")
+         ForEach(0 ..< 4) { index in
+             ExcerciseView(index: index)  // Text("Excercise 1")
+         }
+      }
+     .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+   }
+ }
+
+#Preview {
+  ContentView()
+}
